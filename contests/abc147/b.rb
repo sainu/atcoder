dad0f325eff9854b79d35e4a8ab0c3d16e622af3
sainu <katsutoshi.saino@gmail.com>
2019-12-22 17:20:33 +0900

    Try contest abc147

---
+++ contests/abc147/b.rb
@@ -0,0 +1,16 @@
+s = gets.chomp
+count = 0
+a = s[0...s.length/2].split('')
+b =
+  if s.length.even?
+    s[s.length/2..-1].reverse.split('')
+  else
+    s[s.length/2+1..-1].reverse.split('')
+  end
+a.each.with_index do |chr, i|
+  if chr != b[i]
+    b[i] = chr
+    count += 1
+  end
+end
+puts count
---
+++ contests/abc147/c.rb
@@ -0,0 +1,28 @@
+n = gets.to_i # 人数
+people = []
+n.times do
+  people << []
+  ai = gets.to_i # 証言数
+  ai.times do
+    ans = gets.chomp.split(' ').map(&:to_i)
+    people.last << ans
+  end
+end
+all_answers = []
+people.each.with_index do |answers, index|
+  all_answers << [index, Array.new(n, nil)]
+  answers.each do |ans|
+    all_answers.last.last[ans[0] - 1] = ans[1] > 0
+  end
+end
+p all_answers
+n.downto(0) do |t_count|
+  all_answers.combination(t_count) do |t_answers|
+  tmp_data = Array.new(n, nil)
+    tmp_data.each.with_index do |tmp, i|
+      if tmp.nil?
+        tmp_data[i] = t_answers.last[i]
+      end
+    end
+  end
+end
