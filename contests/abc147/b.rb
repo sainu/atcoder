a8e58d5d117c9b3a03ab173d3a811959e5872fb5
sainu <katsutoshi.saino@gmail.com>
2019-12-22 18:33:46 +0900

    Clean abc147_b

--- contests/abc147/b.rb
+++ contests/abc147/b.rb
@@ -1,17 +1,8 @@
 # https://atcoder.jp/contests/abc147/tasks/abc147_b
 s = gets.chomp
-count = 0
-a = s[0...s.length/2].split('')
-b =
-  if s.length.even?
-    s[s.length/2..-1].reverse.split('')
-  else
-    s[s.length/2+1..-1].reverse.split('')
-  end
-a.each.with_index do |chr, i|
-  if chr != b[i]
-    b[i] = chr
-    count += 1
-  end
+len = s.length
+res = 0
+s.split('').each.with_index do |chr, i|
+  res += 1 if chr != s[len - 1 - i]
 end
-puts count
+puts res / 2
