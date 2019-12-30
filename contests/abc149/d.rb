12399a7bf7fb9b6b3a96931c518de27e046fd315
sainu <katsutoshi.saino@gmail.com>
2019-12-30 18:49:34 +0900

    Rewrite abc149 d as greedy algorithm

--- contests/abc149/d.rb
+++ contests/abc149/d.rb
@@ -22,15 +22,20 @@
 N, K = gets.split(' ').map(&:to_i)
 R, S, P = gets.split(' ').map(&:to_i)
 T = gets.chomp
-wins_pair = {
-  'r' => 'p',
-  's' => 'r',
-  'p' => 's'
-}
-wins_order = {
-  'r' => R,
-  's' => S,
-  'p' => P
-}.sort_by{|k,v|-v}
-cpu_hands = T.split
-my_hands = Array.new(cpu_hands.length, nil)
+
+# 解放1: DP
+
+# 解放2: 貪欲方
+hand_to_score_hash = { 'r' => R, 's' => S, 'p' => P }
+win_hand_map = { 'r' => 'p', 's' => 'r', 'p' => 's' }
+total_score = 0
+N.times do |i|
+  win_hand = win_hand_map[T[i]]
+  add_score = hand_to_score_hash[win_hand]
+  if i >= K && T[i] == T[i - K]
+    add_score = 0
+    T[i] = 'n'
+  end
+  total_score += add_score
+end
+puts total_score
