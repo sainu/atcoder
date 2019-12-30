9db29dd74b53e4dbedecc32cedb419431d4c9268
sainu <katsutoshi.saino@gmail.com>
2019-12-30 18:48:07 +0900

    Rewrite abc149 c for using my prime? function

--- contests/abc149/c.rb
+++ contests/abc149/c.rb
@@ -1,13 +1,21 @@
-require 'prime'
-x = gets.to_i
-t = x
-prime = nil
-loop do
-  if Prime.prime?(t)
-    prime = t
-    break
-  else
-    t += 1
+# 回答1
+# 実行時間: 20 ms
+# メモリ: 2368 KB
+# require 'prime'
+# x = gets.to_i
+# p Prime.find{|y|y>=x}
+
+# 回答2
+# 実行時間: 13 ms
+# メモリ: 1788 KB
+def prime?(x)
+  return false if x <= 1
+  (2..x-1).each do |y|
+    return false if x % y == 0
   end
+  true
 end
+x = gets.to_i
+prime = x
+prime += 1 while !prime?(prime)
 puts prime
